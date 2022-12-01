155027838b19405ceb58b4aec48832ac37a71dbe
Salvador Borit <salvaborit@gmail.com>
2022-12-01 19:00:11 -0300

    .

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,43 +2,22 @@
 """
 Module 0-gather_data_from_an_API
 """
-import json
 import requests
 from sys import argv
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    res = requests.get(
-        "https://jsonplaceholder.typicode.com/todos/?userId={}".format(argv[1])
-    )
-    tsks = json.loads(res.text)
-    usrs = requests.get("https://jsonplaceholder.typicode.com/users")
-    us = json.loads(usrs.text)
-    res_us = {}
-    counter = 0
-    counter_tsks = 0
-    nid = int(argv[1])
+    resp = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos')
+    completed = []
+    for todo_item in resp.json():
+        if todo_item['completed'] == True:
+            completed.append(todo_item['title'])
+    total_tasks = len(resp.json())
+    completed_tasks = len(completed)
+    resp = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+    user_name = resp.json().get('name')
 
-    for line in us:
-        counter += 1
-        res_us.update(line)
-        if counter == nid:
-            break
-    name_us = res_us["name"]
-
-    my_tsks = [
-        (i["completed"], i["title"]) for i in tsks
-        if "completed" or "title" in i
-    ]
-
-    for i, j in my_tsks:
-        if i is True:
-            counter_tsks += 1
-
-    print("Employee {} is done with tasks({}/20):"
-          .format(name_us, counter_tsks))
-
-    for i, j in my_tsks:
-        if i is True:
-            print("\t {}".format(j))
+    print(f'Employee {user_name} is done with tasks ({completed_tasks}/{total_tasks}):')
+    for task in completed:
+        print(f'\t {task}')
