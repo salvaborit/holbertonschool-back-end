0b256992f368b65a844d1bcf8369c3f3708ad85e
Salvador Borit <salvaborit@gmail.com>
2022-12-01 19:03:33 -0300

    .

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,16 +8,16 @@ from sys import argv
 
 if __name__ == '__main__':
 
-    resp = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos')
+    resp = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
     completed = []
     for todo_item in resp.json():
         if todo_item['completed'] == True:
             completed.append(todo_item['title'])
     total_tasks = len(resp.json())
     completed_tasks = len(completed)
-    resp = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+    resp = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
     user_name = resp.json().get('name')
 
-    print(f'Employee {user_name} is done with tasks ({completed_tasks}/{total_tasks}):')
+    print('Employee {} is done with tasks ({}/{}):'.format(user_name, completed_tasks, total_tasks))
     for task in completed:
-        print(f'\t {task}')
+        print('\t {}'.format(task))
