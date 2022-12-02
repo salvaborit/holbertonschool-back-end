d7adb0cdcf081a17548fcf914261848c0b63f060
Salvador Borit <salvaborit@gmail.com>
2022-12-02 11:36:37 -0300

    .

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,18 +2,18 @@
 """
 Module 1-export_to_csv
 """
+import csv
 import requests
 from sys import argv
-import csv
 
-from pprint import pprint
+# from pprint import pprint
 
 
 if __name__ == '__main__':
 
     resp = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
-    user_name = resp.json().get('name')
+    user_name = resp.json().get('username')
 
     resp = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
@@ -22,6 +22,6 @@ if __name__ == '__main__':
         for todo_item in resp.json():
             writer = csv.writer(file)
             writer.writerow([str(todo_item['userId']),
-                             str(user_name.split()[0]),
+                             str(user_name),
                              str(todo_item['completed']),
                              str(todo_item['title'])])
