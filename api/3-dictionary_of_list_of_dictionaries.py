49ee564fb5371c2a90111881961bff2f6c3cf15a
Salvador Borit <salvaborit@gmail.com>
2022-12-02 14:21:40 -0300

    .

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module 2-export_to_json
+"""
+from json import dump
+import requests
+
+from pprint import pprint
+
+
+if __name__ == '__main__':
+
+    users_response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/')
+    todos_response = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/')
+
+    users = {}
+    for user in users_response.json():
+        users[user['id']] = user['username']
+
+    tasks = {}
+    for uid, uname in users.items():
+        for todo in todos_response.json():
+            if todo['userId'] == uid:
+                tasks.setdefault(uid, [])
+                tasks[uid].append(dict(username=uname,
+                                       task=todo['title'],
+                                       completed=todo['completed']))
+
+    with open('todo_all_employees.json', 'w') as file:
+        dump(tasks, file)
